e9fd29986a940fbf467c6e96d68b8b9f0964801f
krishansharma009 <167859508+krishansharma009@users.noreply.github.com>
2024-07-23 16:40:01 +0530

    Update data-source.js

--- config/data-source.js
+++ config/data-source.js
@@ -1,26 +1,26 @@
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config(); // Load environment variables from .env file
+const { Sequelize } = require("sequelize");
+require("dotenv").config(); // Load environment variables from .env file
 
-// const sequelize = new Sequelize(
-//   process.env.RDS_DB_NAME || 'bgv',
-//   process.env.RDS_USERNAME,
-//   process.env.RDS_PASSWORD,
-//   {
-//     host: process.env.RDS_HOSTNAME,
-//     post: process.env.RDS_PORT,
-//     dialect: "mysql",
-//     dialectModule: require("mysql2"), // Explicitly use mysql2 for Sequelize
-//   }
-// );
+const sequelize = new Sequelize(
+  process.env.RDS_DB_NAME || 'bgv',
+  process.env.RDS_USERNAME,
+  process.env.RDS_PASSWORD,
+  {
+    host: process.env.RDS_HOSTNAME,
+    post: process.env.RDS_PORT,
+    dialect: "mysql",
+    dialectModule: require("mysql2"), // Explicitly use mysql2 for Sequelize
+  }
+);
 
-// // Test the connection
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//   })
-//   .catch((err) => {
-//     console.error("Unable to connect to the database:", err);
-//   });
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-// module.exports = sequelize;
+module.exports = sequelize;
