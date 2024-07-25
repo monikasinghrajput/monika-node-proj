17a60f673d98c89476d94c77f71ac140636e85c1
krishansharma009 <167859508+krishansharma009@users.noreply.github.com>
2024-07-25 21:04:33 +0530

    config update

--- config/mailer.js
+++ config/mailer.js
@@ -3,13 +3,9 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "live.smtp.mailtrap.io",
   port: 587,
-  secure: true,
   auth: {
     user: "api",
-    pass: "f9f05dca0e3c3c1918179103fc71b7c1",
-  },
-  tls: {
-    rejectUnauthorized: false  // Allow self-signed certificates
+    pass: "f9f05dca0e3c3c1918179103fc71b7c1"
   }
 });
 module.exports = transporter;
