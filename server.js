c00bd5a6b7b60635f06722c9cb26926ef9c630dd
krishansharma009 <godkrihna.apple.mtt@gmail.com>
2024-08-22 12:34:54 +0530

    server.js m changes

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(passport.initialize());
 const path=require('path')
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));//
 
 // Comment out the authentication middleware
 /*
