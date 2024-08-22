4d8fd632edb9985e6e34b9d6d653af2023ab4830
krishansharma009 <godkrihna.apple.mtt@gmail.com>
2024-08-22 12:08:33 +0530

    cibil create to update one

--- api/candidate-cibil/candidate-cibil-controller.js
+++ api/candidate-cibil/candidate-cibil-controller.js
@@ -2,8 +2,44 @@ const CandidteCibil = require("./candidte-cibil"); // Ensure the correct path
 const REST_API = require("../../util/api-util");
 
 // Define the createCandidate controller function
+// const createCandidateCibil = async (req, res) => {
+
+//   const response = await REST_API._add(req, res, CandidteCibil);
+//   res.status(200).json(response);
+// };
+
 const createCandidateCibil = async (req, res) => {
-  const response = await REST_API._add(req, res, CandidteCibil);
+  let response;
+  const { ...updateData } = req.body;
+  let update = updateData[0];
+  let candidate_id = update.candidate_id;
+  let id = update.id;
+  console.log(id, candidate_id);
+
+  console.log("updateData");
+  console.log(req.body);
+
+  const existEdu = await CandidteCibil.findOne({
+    where: { candidate_id, id },
+  });
+  console.log("data");
+
+  if (!existEdu) {
+    return res.status(404).json({ error: "Cibil not found" });
+  }
+
+  const [updatedRows] = await CandidteCibil.update(
+    {
+      pan_number: req.body.pan_number,
+      cibil_score: req.body.cibil_score,
+      aadhar_number: req.body.aadhar_number,
+    },
+    {
+      where: { candidate_id, id },
+    }
+  );
+  console.log(updatedRows);
+  response = { id, updated: updatedRows > 0 };
   res.status(200).json(response);
 };
 
@@ -29,6 +65,7 @@ const updateCandidteCibil = async (req, res) => {
 
   let response;
   const { candidate_id, id, ...updateData } = req.body;
+  console.log(updateData);
 
   console.log("updateData");
   const existEdu = await CandidteCibil.findOne({
@@ -37,7 +74,7 @@ const updateCandidteCibil = async (req, res) => {
   console.log(existEdu, "data");
 
   if (!existEdu) {
-    return res.status(404).json({ error: "Education not found" });
+    return res.status(404).json({ error: "Cibil not found" });
   }
 
   const [updatedRows] = await CandidteCibil.update(updateData, {
@@ -46,7 +83,6 @@ const updateCandidteCibil = async (req, res) => {
   console.log(id);
   response = { id, updated: updatedRows > 0 };
   res.status(200).json(response);
-
 };
 
 const deleteCandidateCibil = async (req, res) => {
--- api/candidate-cibil/candidate-cibil-route.js
+++ api/candidate-cibil/candidate-cibil-route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const candidateCibilController = require('./candidate-cibil-controller'); // Ensure the correct path and export
 
 // Define a route for creating a user
- router.post('/', candidateCibilController.createCandidateCibil);
+ router.post("/", candidateCibilController.createCandidateCibil); //createCandidateCibil
 router.put('/', candidateCibilController.updateCandidteCibil); 
 
 router.get('/', candidateCibilController.getCandidteListCibil);
