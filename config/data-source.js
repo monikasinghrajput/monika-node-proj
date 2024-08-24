67b88587c8eafb91bdbd3b35c2c181139e4ee5d1
monikasinghrajput <ms6139087@gmail.com>
2024-08-24 12:54:00 +0530

    tets

--- config/data-source.js
+++ config/data-source.js
@@ -1,42 +1,26 @@
-const { Sequelize } = require('sequelize');
-const AWS = require('aws-sdk');
+const { Sequelize } = require("sequelize");
+require("dotenv").config(); // Load environment variables from .env file
 
-const SecretsManager = new AWS.SecretsManager();
-
-async function getRDSSecrets() {
-  const secretName = 'rds-db-credentials/awseb-e-i6edbiy8te-stack-awsebrdsdatabase-viwwidl6767v/admin/1724434328596';
-  try {
-    const data = await SecretsManager.getSecretValue({ SecretId: secretName }).promise();
-    if ('SecretString' in data) {
-      return JSON.parse(data.SecretString);
-    }
-  } catch (err) {
-    console.error('Error retrieving secret:', err);
-    throw err;
+const sequelize = new Sequelize(
+  process.env.RDS_DB_NAME || 'bgv',
+  process.env.RDS_USERNAME,
+  process.env.RDS_PASSWORD,
+  {
+    host: process.env.RDS_HOSTNAME,
+    post: process.env.RDS_PORT,
+    dialect: "mysql",
+    dialectModule: require("mysql2"), // Explicitly use mysql2 for Sequelize
   }
-}
+);
 
-(async () => {
-  const secrets = await getRDSSecrets();
-  const sequelize = new Sequelize(
-    process.env.RDS_DB_NAME,
-    secrets.username,
-    secrets.password,
-    {
-      host: process.env.RDS_HOSTNAME,
-      port: process.env.RDS_PORT || 3306,
-      dialect: 'mysql',
-      dialectModule: require('mysql2'),
-    }
-  );
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-  // Test the connection
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (err) {
-    console.error('Unable to connect to the database:', err);
-  }
-
-  module.exports = sequelize;
-})();
+module.exports = sequelize;
