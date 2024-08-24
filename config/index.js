fc1384a72dd021e1ac050cdf4bbaa21cd2c69340
monikasinghrajput <ms6139087@gmail.com>
2024-08-24 21:49:32 +0530

    test

--- config/index.js
+++ config/index.js
@@ -1,22 +1,9 @@
-const setupDatabase = require('./db'); // Adjust the path as needed
-const logger = require('./logger');
-const transporter = require('./mailer');
+const db = require("./data-source");
+const logger = require("./logger");
+const transporter = require("./mailer");
 
-let db; // Define a variable to hold the database connection
 
-// Initialize the database connection
-(async () => {
-  try {
-    db = await setupDatabase();
-    console.log('Database connection established.');
-  } catch (error) {
-    console.error('Failed to initialize database:', error);
-  }
-})();
 
-// Export the initialized modules
-module.exports = {
-  db,
-  logger,
-  transporter,
-};
+exports.db = db;
+exports.logger = logger;
+exports.transporter = transporter;
