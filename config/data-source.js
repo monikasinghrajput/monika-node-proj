946e9a80f9588b03bd2eaeb7f29514882d63a276
monikasinghrajput <ms6139087@gmail.com>
2024-08-24 11:29:17 +0530

    tets

--- config/data-source.js
+++ config/data-source.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.RDS_PASSWORD,
   {
     host: process.env.RDS_HOSTNAME,
-    post: process.env.RDS_PORT,
+    port: process.env.RDS_PORT,
     dialect: "mysql",
     dialectModule: require("mysql2"), // Explicitly use mysql2 for Sequelize
   }
